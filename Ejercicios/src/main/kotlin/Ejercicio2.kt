feee7e2abc5dc10bb408be0a260312a35d559021
sebasdelalv340 <sdelgado@iesrafaelalberti.es>
2023-12-13 17:43:19 +0100

    version inicial

--- Ejercicios/src/main/kotlin/Ejercicio2.kt
+++ Ejercicios/src/main/kotlin/Ejercicio2.kt
@@ -1,4 +1,3 @@
-
 fun ejU6() {
     /* Escribe un programa que pida el importe final de un artículo y calcule e imprima por pantalla el
     IVA que se ha pagado y el importe sin IVA (suponiendo que se ha aplicado un tipo de IVA del 10%) */
--- Ejercicios/src/main/kotlin/Main.kt
+++ Ejercicios/src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 fun main() {
     //ejU4()
     //ejU6()
-    ejU12()
+    //ejU12()
+    //ejU15()
+    ejU18()
 
 }
