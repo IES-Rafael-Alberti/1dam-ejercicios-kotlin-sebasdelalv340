1ee3c34cde580c502f27c1d31ddabd755ffb3d02
sebasdelalv340 <sdelgado@iesrafaelalberti.es>
2023-12-24 12:58:06 +0100

    commint

--- Ejercicios/src/main/kotlin/Ejercicio5.kt
+++ Ejercicios/src/main/kotlin/Ejercicio5.kt
@@ -1,4 +1,3 @@
-import java.util.*
 
 fun ejU18() {
     //Escribir un programa que pregunte el nombre completo del usuario en la consola y después muestre por pantalla el nombre completo del usuario tres veces, una con todas las letras minúsculas, otra con todas las letras mayúsculas y otra solo con la primera letra del nombre y de los apellidos en mayúscula. El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera.
@@ -6,15 +5,21 @@ fun ejU18() {
     println("Introduce tu nombre completo: ")
 
     try {
-        var nombre = readln().toString()
-        val str = nombre.split(" ")
-            .joinToString(" ") { it.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() } }
+        val nombre = readln().toString()
 
         println(nombre.lowercase())
         println(nombre.uppercase())
-        println(str)
-    }
-    catch (e:Exception) {
+        letraMayus(nombre)
+
+    } catch (e: Exception) {
         println("entrada no válida")
     }
 }
+fun letraMayus(nombre: String) {
+    val str = nombre.split(" ")
+
+    for (i in str) {
+        print("${i.replaceFirstChar{ it.uppercase() }} ")
+    }
+}
+
--- Ejercicios/src/main/kotlin/Main.kt
+++ Ejercicios/src/main/kotlin/Main.kt
@@ -1,8 +1,40 @@
+
 fun main() {
     //ejU4()
     //ejU6()
     //ejU12()
     //ejU15()
-    ejU18()
+    //ejU18()
+    //ejU20()
+    //ejU21()
+    //ejU22()
+    //ejU24()
+    //ejU25()
+    //ejU26()
+    ejU27()
+}
 
+fun mostrarMenuPpal() {
+    println("1. Ejercicios U1")
+    println("2. Ejercicios U2")
+    println("3. Ejercicios U3")
+}
+
+fun pedirOpcion(min: Int, max: Int): Int {
+
+    var opcion: Int
+    do {
+        println("Seleccione opción (0 = SALIR) >> ")
+        opcion = try {
+            readln().toInt()
+        } catch (e: NumberFormatException) {
+            println("entrada no válida")
+            -1
+        }
+        if (opcion !in min..max) {
+            println("entrada no válida")
+        }
+    } while (opcion !in min..max)
+
+    return opcion
 }
